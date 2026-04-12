fc68d2606e41b995ca83a328b99edbde52c02d8a
Srimathi <m.srimathi@gmail.com>
2026-04-12 23:15:09 +0530

    Fixed Firebase API key

--- website/frontend/firebase-messaging-sw.js
+++ website/frontend/firebase-messaging-sw.js
@@ -2,12 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
- apiKey: "AIzaSyAyp...",
+ apiKey: "AIzaSyAYpXGDlYTg03U-WWRpEvIAqnqUyJFc6QU",
   authDomain: "lab-booking-system-e77ad.firebaseapp.com",
   projectId: "lab-booking-system-e77ad",
   storageBucket: "lab-booking-system-e77ad.firebasestorage.app",
   messagingSenderId: "492426985882",
-  appId: "1:492426985882:web:c48566f44e48acdb497a23"
+  appId: "1:492426985882:web:648566f44e48acdb497a23",
+  measurementId: "G-Z8H51WZZYP"
 });
 
 const messaging = firebase.messaging();
