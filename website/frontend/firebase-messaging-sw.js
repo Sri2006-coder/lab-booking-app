37f19a0d3a8363f3badba476397538d80b2c399a
Srimathi <m.srimathi@gmail.com>
2026-04-12 23:35:14 +0530

    Fixed Firebase push notification setup

--- website/frontend/firebase-messaging-sw.js
+++ website/frontend/firebase-messaging-sw.js
@@ -2,13 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
- apiKey: "AIzaSyAYpXGDlYTg03U-WWRpEvIAqnqUyJFc6QU",
+  apiKey: "AIzaSyAYpXGDlYTg03U-WWRpEvIAqnqUyJFc6QU",
   authDomain: "lab-booking-system-e77ad.firebaseapp.com",
   projectId: "lab-booking-system-e77ad",
   storageBucket: "lab-booking-system-e77ad.firebasestorage.app",
   messagingSenderId: "492426985882",
-  appId: "1:492426985882:web:648566f44e48acdb497a23",
-  measurementId: "G-Z8H51WZZYP"
+  appId: "1:492426985882:web:648566f44e48acdb497a23"
 });
 
 const messaging = firebase.messaging();
